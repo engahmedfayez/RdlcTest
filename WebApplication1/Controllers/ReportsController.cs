784b367d6e85701117949dc0ad5366e17335be0d
engahmedfayez <eng.ahfayez@google.com>
2025-07-17 13:15:09 +0300

    rdlc0

--- WebApplication1/Controllers/ReportsController.cs
+++ WebApplication1/Controllers/ReportsController.cs
@@ -13,37 +13,32 @@ namespace WebApplication1.Controllers
 {
     public class ReportsController : Controller
     {
-
-
         private readonly IConfiguration _config;
 
-        public ReportsController (IConfiguration config)
+        public ReportsController(IConfiguration config)
         {
             _config = config;
-
         }
 
 
         [HttpPost]
         [Route("Reports/viewer")]
-        public IActionResult GenerateRDLCReport ([FromBody] ReportInput reportInput)
+        public IActionResult GenerateRDLCReport([FromBody] ReportInput reportInput)
         {
             if (!ModelState.IsValid) return BadRequest(ModelState);
 
-             const string ReportDataSourceName = "Items";
-             const string ReportFormat = "PDF";
-             const string MimeTypePdf = "application/pdf";
-             var HospitalName = _config.GetSection("ReportSettings")["HospitalName"];
-             var LogoPath     = _config.GetSection("ReportSettings")["LogoPath"];
-
-
+            const string ReportDataSourceName = "Items";
+            const string ReportFormat = "PDF";
+            const string MimeTypePdf = "application/pdf";
+            var HospitalName = _config.GetSection("ReportSettings")["HospitalName"];
+            var LogoPath = _config.GetSection("ReportSettings")["LogoPath"];
 
             // 2. Load RDLC report
-            var reportPath = Path.Combine(AppContext.BaseDirectory, "Reports" ,  $"{reportInput.ReportName}.rdlc");//"Report.rdlc"
+            var reportPath = Path.Combine(AppContext.BaseDirectory, "Reports", $"{reportInput.ReportName}.rdlc");//"Report.rdlc"
 
-           
-               
 
+
+            // 3. Check if the report file exists
             if (!System.IO.File.Exists(reportPath))
             {
                 return NotFound($"Report file ['{reportInput.ReportName}.rdlc'] not found.");
@@ -53,11 +48,11 @@ namespace WebApplication1.Controllers
 
 
             #region Set Header Data
-
+            // 4. Set report header data
             DataTable headerDataTable = new DataTable();
             headerDataTable.Columns.Add("HospitalName", typeof(string));
-            headerDataTable.Columns.Add("ImageByte"   , typeof(byte[]));
-
+            headerDataTable.Columns.Add("ImageByte", typeof(byte[]));
+            // Add a row with the hospital name and logo
             headerDataTable.Rows.Add(HospitalName, System.IO.File.ReadAllBytes(LogoPath));
             localReport.DataSources.Add(new ReportDataSource("HeaderData", headerDataTable));
 
@@ -87,7 +82,7 @@ namespace WebApplication1.Controllers
             {
                 return StatusCode(500, $"Internal Server Error [Report Data] : {ex.Message}");
             }
-                       
+
             try
             {
                 reportArgs = JsonConvert.DeserializeObject<List<string>>(ReportArgsJson);
@@ -96,9 +91,9 @@ namespace WebApplication1.Controllers
                     return BadRequest("Report arguments cannot be null or empty.");
                 }
                 // Convert list to ReportParameter array
-                  parameters = reportArgs
-                 .Select((value, index) => new ReportParameter($"arg{index}", value))
-                 .ToArray();
+                parameters = reportArgs
+               .Select((value, index) => new ReportParameter($"arg{index}", value))
+               .ToArray();
 
 
             }
@@ -106,20 +101,20 @@ namespace WebApplication1.Controllers
             {
 
                 return StatusCode(500, $"Internal Server Error [Report Args] : {ex.Message}");
-            }  
+            }
             try
             {
                 // 5. Render to PDF
-                localReport.SetParameters(parameters);               
+                localReport.SetParameters(parameters);
                 var result = localReport.Render(ReportFormat);
-                return File(result,  MimeTypePdf , $@"{reportInput.ReportName}.pdf");
+                return File(result, MimeTypePdf, $@"{reportInput.ReportName}.pdf");
             }
             catch (Exception ex)
             {
 
                 return StatusCode(500, "An unexpected error occurred while generating the report [Exception Details] ." + ex.Message);
             }
-             
+
         }
 
     }
